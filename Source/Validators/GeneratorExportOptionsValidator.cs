18027d3bbc250bf0db98ccc4b9fb9c195ce95984
Malcolm Smith <malcolm@mjfreelancing.com>
2023-07-28 23:44:11 +1000

    Validator updates

---
+++ Source/Validators/GeneratorExportOptionsValidator.cs
@@ -0,0 +1,32 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Validation;
+using AllOverIt.Validation.Extensions;
+using FluentValidation;
+using SlnDependencyDiagramGenerator.Config;
+using System.IO;
+
+namespace SlnDependencyDiagramGenerator.Validators
+{
+    internal sealed class GeneratorExportOptionsValidator : ValidatorBase<IGeneratorExportOptions>
+    {
+        static GeneratorExportOptionsValidator()
+        {
+            DisablePropertyNameSplitting();
+        }
+
+        public GeneratorExportOptionsValidator()
+        {
+            RuleFor(model => model.Path).IsNotEmpty();
+
+            When(model => model.Path.IsNotNullOrEmpty(), () =>
+            {
+                RuleFor(model => model.Path)
+                    .Must(Directory.Exists)
+                    .WithMessage("The export path was not found.");
+            });
+
+            RuleFor(model => model.ImageFormats).NotNull();
+        }
+    }
+
+}
