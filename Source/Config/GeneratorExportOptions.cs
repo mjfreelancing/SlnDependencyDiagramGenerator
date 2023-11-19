ee219f7b31186ea032ceb31520d96f0a93a38e35
Malcolm Smith <malcolm@mjfreelancing.com>
2023-11-19 22:59:19 +1100

    Updates for RC 13

--- Source/Config/GeneratorExportOptions.cs
+++ Source/Config/GeneratorExportOptions.cs
@@ -6,11 +6,12 @@ namespace SlnDependencyDiagramGenerator.Config
     /// <summary>Specifies export path and image format options.</summary>
     public sealed class GeneratorExportOptions
     {
-        /// <summary>Clears the contents of the <see cref="Path"/> when true.</summary>
+        /// <summary>When <see langword="True"/>, clears the contents of the folder that combines <see cref="RootPath"/>
+        /// with the target framework being processed.</summary>
         public bool ClearContents { get; init; }
 
         /// <summary>The fully-qualified export root path for the generated diagram files and images.
-        /// A sub-folder will be created for each targetFramework processed.</summary>
+        /// A sub-folder will be created for each target framework processed.</summary>
         public string RootPath { get; init; }
 
         /// <summary>The diagram image formats to create. Can be empty or one or more of "png", "svg", "pdf".</summary>
