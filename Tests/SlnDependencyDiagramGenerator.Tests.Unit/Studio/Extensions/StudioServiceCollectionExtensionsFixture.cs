16f8f28ce83bf84ad5d9d4699c2e17f7a5883f12
Malcolm Smith <malcolm@mjfreelancing.com>
2026-06-19 20:34:32 +1000

    Removed StudioGenerationService related code - was created early and does not all align with intent

--- Tests/SlnDependencyDiagramGenerator.Tests.Unit/Studio/Extensions/StudioServiceCollectionExtensionsFixture.cs
+++ Tests/SlnDependencyDiagramGenerator.Tests.Unit/Studio/Extensions/StudioServiceCollectionExtensionsFixture.cs
@@ -3,27 +3,11 @@ using Shouldly;
 using SlnDependencyDiagramGenerator.Generator;
 using SlnDependencyStudio.Shared.DependencyInjection;
 using SlnDependencyStudio.Shared.Extensions;
-using SlnDependencyStudio.Shared.Services;
 
 namespace SlnDependencyDiagramGenerator.Tests.Unit.Studio.Extensions;
 
 public class StudioServiceCollectionExtensionsFixture
 {
-    [Fact]
-    public void Should_Resolve_IStudioGenerationService_After_AddStudioServices()
-    {
-        var services = new ServiceCollection();
-        services.AddSingleton(new DependencyGenerator());
-        services.AddSlnDependency();
-
-        var provider = services.BuildServiceProvider();
-
-        using var scope = provider.CreateScope();
-        var generationService = scope.ServiceProvider.GetService<IStudioGenerationService>();
-
-        generationService.ShouldNotBeNull();
-    }
-
     [Fact]
     public void Should_Not_Register_Marker_Interface_As_Service()
     {
